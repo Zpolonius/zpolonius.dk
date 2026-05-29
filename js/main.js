edecfc5394dcf183e4f5f723becdafd47fba9113
zpolonius <zacharias@polonius.dk>
2026-05-29 22:56:01 +0200

    Fix Lighthouse cookie issues by implementing a facade for Spotify embeds

--- js/main.js
+++ js/main.js
@@ -143,10 +143,21 @@ function loadDeferredContent() {
     const type = container.getAttribute('data-type');
     const src = container.getAttribute('data-src');
     const title = container.getAttribute('data-title');
-    
+
     if (type === 'spotify') {
+      // Vis en facade i stedet for iframen med det samme
       container.innerHTML = `
-        <iframe title="${esc(title)}" style="border-radius:12px" src="${src}" width="100%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <div class="spotify-facade" onclick="this.parentElement.innerHTML='<iframe title=\'${esc(title)}\' style=\'border-radius:12px\' src=\'${src}&autoplay=1\' width=\'100%\' height=\'152\' frameBorder=\'0\' allow=\'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\' loading=\'lazy\' sandbox=\'allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation\'></iframe>'">
+          <div class="facade-content">
+            <div class="facade-play-btn">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M8 5v14l11-7z"/></svg>
+            </div>
+            <div class="facade-info">
+              <div class="facade-title">${esc(title)}</div>
+              <div class="facade-sub">Klik for at indlæse afspiller fra Spotify</div>
+            </div>
+          </div>
+        </div>
       `;
     }
   });
